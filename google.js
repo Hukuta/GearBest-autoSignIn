dbeea933c571d00ad147bad1f5a4c087b5f49f71
Odarchenko N.D <odarchenko.n.d@gmail.com>
2017-11-02 20:42:08 +0200

    wait for loading

--- google.js
+++ google.js
@@ -1,13 +1,21 @@
 var authorize = function () {
-    gapi.auth.authorize({
-        'approvalprompt': 'force',
-        'client_id': '643156394993-h15vso2j5acka94qgvl273mqun0083q3.apps.googleusercontent.com',
-        'height': 'long',
-        'cookiepolicy': 'https://gearbest.com',
-        'requestvisibleactions': 'http://schemas.google.com/AddActivity',
-        'redirecturi': 'postmessage',
-        'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'
-    }, window.loginFinishedCallback);
+    var gapiInterval = setInterval(function () {
+        if (typeof window['gapi'] == 'undefined')
+            return null;
+
+        clearInterval(gapiInterval);
+
+        gapi.auth.authorize({
+            'approvalprompt': 'force',
+            'client_id': '643156394993-h15vso2j5acka94qgvl273mqun0083q3.apps.googleusercontent.com',
+            'height': 'long',
+            'cookiepolicy': 'https://gearbest.com',
+            'requestvisibleactions': 'http://schemas.google.com/AddActivity',
+            'redirecturi': 'postmessage',
+            'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'
+        }, window.loginFinishedCallback);
+    }, 250);
+
 };
 
 var d = document.createElement('script');
